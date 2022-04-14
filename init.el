f4a01d342ce761c89b44b357a51917d2c4bf352b
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-04-14 15:37:27 +0530

    company: reset some config to bare bones company
    the config was constantly messing with yas-snippets

--- init.el
+++ init.el
@@ -468,22 +468,10 @@
 
 ;; auto complete
 (require 'cc-mode)
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-backends (delete 'company-semantic company-backends))
 
-;; Add yasnippet support for all company backends
-;; https://github.com/syl20bnr/spacemacs/pull/179
-(defvar company-mode/enable-yas t
-  "Enable yasnippet for all backends.")
-
-(defun company-mode/backend-with-yas (backend)
-  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-      backend
-    (append (if (consp backend) backend (list backend))
-            '(:with company-yasnippet))))
-
-(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+;; (require 'company)
+;;(add-hook 'after-init-hook 'global-company-mode)
+;;(setq company-backends (delete 'company-semantic company-backends))
 
 ;;company-c-headers
 (require 'company-c-headers)
