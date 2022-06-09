50506f55a9eb8ad8c30c75df149c5212f5e70d01
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-06-09 12:24:06 +0530

    Org: revert a lot of settings that I don't use
    - fix some formatting in comments
    - revert a lot of org settings ( might add them back in future )

--- init.el
+++ init.el
@@ -46,7 +46,6 @@
 
 (use-package doom-themes
     :init (load-theme 'doom-horizon t))
-;; earlier theme doom-palenight
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package Manager Config ;;
@@ -228,110 +227,8 @@
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
 
-  (setq org-agenda-files
-	'("~/.emacs.d/org_files/tasks.org"
-	  "~/.emacs.d/org_files/birthdays.org"
-          "~/.emacs.d/org_files/habits.org"))
-
-  (require 'org-habit)
-  (add-to-list 'org-modules 'org-habit)
-  (setq org-habit-graph-column 60)
-
-  (setq org-todo-keywords
-    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-      (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
-
-  (setq org-refile-targets
-    '(("archive.org" :maxlevel . 1)
-      ("tasks.org" :maxlevel . 1)))
-
   ;; Save Org buffers after refiling!
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
-
-  (setq org-tag-alist
-    '((:startgroup)
-       ; Put mutually exclusive tags here
-       (:endgroup)
-       ("@errand" . ?E)
-       ("@home" . ?H)
-       ("@work" . ?W)
-       ("agenda" . ?a)
-       ("planning" . ?p)
-       ("publish" . ?P)
-       ("batch" . ?b)
-       ("note" . ?n)
-       ("idea" . ?i)))
-
-  ;; Configure custom agenda views
-  (setq org-agenda-custom-commands
-   '(("d" "Dashboard"
-     ((agenda "" ((org-deadline-warning-days 7)))
-      (todo "NEXT"
-        ((org-agenda-overriding-header "Next Tasks")))
-      (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
-
-    ("n" "Next Tasks"
-     ((todo "NEXT"
-        ((org-agenda-overriding-header "Next Tasks")))))
-
-    ("W" "Work Tasks" tags-todo "+work-email")
-
-    ;; Low-effort next actions
-    ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-     ((org-agenda-overriding-header "Low Effort Tasks")
-      (org-agenda-max-todos 20)
-      (org-agenda-files org-agenda-files)))
-
-    ("w" "Workflow Status"
-     ((todo "WAIT"
-            ((org-agenda-overriding-header "Waiting on External")
-             (org-agenda-files org-agenda-files)))
-      (todo "REVIEW"
-            ((org-agenda-overriding-header "In Review")
-             (org-agenda-files org-agenda-files)))
-      (todo "PLAN"
-            ((org-agenda-overriding-header "In Planning")
-             (org-agenda-todo-list-sublevels nil)
-             (org-agenda-files org-agenda-files)))
-      (todo "BACKLOG"
-            ((org-agenda-overriding-header "Project Backlog")
-             (org-agenda-todo-list-sublevels nil)
-             (org-agenda-files org-agenda-files)))
-      (todo "READY"
-            ((org-agenda-overriding-header "Ready for Work")
-             (org-agenda-files org-agenda-files)))
-      (todo "ACTIVE"
-            ((org-agenda-overriding-header "Active Projects")
-             (org-agenda-files org-agenda-files)))
-      (todo "COMPLETED"
-            ((org-agenda-overriding-header "Completed Projects")
-             (org-agenda-files org-agenda-files)))
-      (todo "CANC"
-            ((org-agenda-overriding-header "Cancelled Projects")
-             (org-agenda-files org-agenda-files)))))))
-
-  (setq org-capture-templates
-    `(("t" "Tasks / Projects")
-      ("tt" "Task" entry (file+olp "~/.emacs.d/org_files/tasks.org" "Inbox")
-           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
-
-      ("j" "Journal Entries")
-      ("jj" "Journal" entry
-           (file+olp+datetree "~/.emacs.d/org_files/jurnol.org")
-           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
-           :clock-in :clock-resume
-           :empty-lines 1)
-      ("jm" "Meeting" entry
-           (file+olp+datetree "~/.emacs.d/org_files/jurnol.org")
-           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-           :clock-in :clock-resume
-           :empty-lines 1)
-
-      ("w" "Workflows")
-      ("we" "Checking Email" entry (file+olp+datetree "~/.emacs.d/org_files/jurnol.org")
-           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)))
-  (efs/org-font-setup))
+  (advice-add 'org-refile :after 'org-save-all-org-buffers))
 
 (use-package org-bullets
   :after org
@@ -346,23 +243,6 @@
 
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
-
-(org-babel-do-load-languages
-  'org-babel-load-languages
-  '((emacs-lisp . t)
-    (python . t)))
-
-(push '("conf-unix" . conf-unix) org-src-lang-modes)
-
-;; Automatically tangle our Emacs.org config file when we save it
-(defun efs/org-babel-tangle-config ()
-  (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/Projects/Code/emacs-from-scratch/Emacs.org"))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
-;;(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
 ;; This is needed as of Org 9.2
 (require 'org-tempo)
@@ -400,22 +280,22 @@
 
 (setq apropos-sort-by-scores t)
 
-;;follow gnu coding convention by defult
+;; follow gnu coding convention by defult
 (setq c-default-style "gnu")
 
 ;;activate whitespace-mode to view all whitespace characters
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 
-;;show unncessary whitespace ( it's more of a distraction for me rn)
+;; show unncessary whitespace ( it's more of a distraction for me rn)
 ;;(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 
-;;use space to indent by default
+;; use space to indent by default
 (setq-default indent-tabs-mode nil)
 
-;;set appearance of a tab that is represented by 8 spaces
+;; set appearance of a tab that is represented by 8 spaces
 (setq-default tab-width 8)
 
-;;dtrt-indent: to detect and set indend offset depending on the file being edited
+;; dtrt-indent: to detect and set indend offset depending on the file being edited
 (require 'dtrt-indent)
 (dtrt-indent-mode 1)
 
@@ -444,18 +324,18 @@
 (use-package ws-butler
   :hook (prog-mode . ws-butler-mode))
 
-;;smartparens
+;; add closing parenthesis with opening ones
 (require 'smartparens-config)
 (show-smartparens-global-mode +1)
 (smartparens-global-mode 1)
 
-;;on RET press, the curly braces automatically add another newline
+;; on RET press, the curly braces automatically add another newline
 (sp-with-modes '(c-mode c++-mode)
   (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
   (sp-local-pair "/*" "*/" :post-handlers '((" | " "SPC")
                                             ("* ||\n[i]" "RET"))))
 
-;; ggtags ( maybe switch to rtags in future )
+;; ggtags (gnu global tags)
 (require 'ggtags)
 (add-hook 'c-mode-common-hook
           (lambda ()
@@ -481,11 +361,11 @@
 ;; auto complete
 (require 'cc-mode)
 
-;; (require 'company)
-;;(add-hook 'after-init-hook 'global-company-mode)
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
 ;;(setq company-backends (delete 'company-semantic company-backends))
 
-;;company-c-headers
+;; company-c-headers ( auto complete c header names )
 (require 'company-c-headers)
 (add-to-list 'company-backends 'company-c-headers)
 ;; let company c headers also complete c++ headers
