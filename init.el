97b91f25c2aa1b6381561e72e6fde8d4fc5cd558
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-06-16 13:57:05 +0530

    UI: configure smooth scrolling

--- init.el
+++ init.el
@@ -32,6 +32,20 @@
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
 
+;; Vertical Scroll
+(setq scroll-step 1)
+(setq scroll-margin 1)
+(setq scroll-conservatively 101)
+(setq scroll-up-aggressively 0.01)
+(setq scroll-down-aggressively 0.01)
+(setq auto-window-vscroll nil)
+(setq fast-but-imprecise-scrolling nil)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+;; Horizontal Scroll
+(setq hscroll-step 1)
+(setq hscroll-margin 1)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Font Configuration ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
