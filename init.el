9a31ff913d72f02464d3837227ba91cac286f772
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-06-16 13:26:16 +0530

    Doom-theme: switch-theme and some other config
    - Add function to switch theme on the go
    - Flashing mode-line on errors
    - Change coursor background
    - Doom theme org config

--- init.el
+++ init.el
@@ -45,7 +45,19 @@
 (set-face-attribute 'variable-pitch nil :font "Cantarell" :height 152 :weight 'regular)
 
 (use-package doom-themes
-    :init (load-theme 'doom-gruvbox t))
+  :custom-face
+  (cursor ((t (:background "BlanchedAlmond"))))
+  :config
+  ;; flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config)
+  (load-theme 'doom-one t)
+  (defun switch-theme ()
+    "An interactive funtion to switch themes."
+    (interactive)
+    (disable-theme (intern (car (mapcar #'symbol-name custom-enabled-themes))))
+    (call-interactively #'load-theme)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package Manager Config ;;
