b6ad4351b7b0dc4d5ab18cba613832737f6a9a30
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-06-20 13:20:42 +0530

    elisp: add ui configuration init files
    - init-dashboard
    - init-theme
    - init-fonts
    - init-ui-config

---
+++ elisp/init-ui-config.el
@@ -0,0 +1,94 @@
+;;; init-ui-config.el ---  -*- lexical-binding: t -*-
+;;
+;; Filename: init-ui-config.el
+;; Description: Initialise ui configuration
+;; Author: Ankur Saini
+;; Copyright (C) 2019 Ankur Saini
+;; Created: Mon Jun 20 09:58:41 2022 (+0530)
+;; Version: 1.0
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 18
+;; URL: https://github.com/Arsenic-ATG/Emacs-config
+;; Keywords: .emacs.d ui
+;; Compatibility: emacs-version >= 26.1
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;; This file initialise UI of the Emacs
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+;; Clean UI
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+;; Use "y/n" instead of "yes/no" prompt
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq use-dialog-box nil)
+
+;; Display line number and colum number
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Display time and battery status in modeline
+(display-time-mode 1)
+(display-battery-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; Smooth scrolling ;;
+;;;;;;;;;;;;;;;;;;;;;;
+
+;; Vertical Scroll
+(setq scroll-step 1)
+(setq scroll-margin 1)
+(setq scroll-conservatively 101)
+(setq scroll-up-aggressively 0.01)
+(setq scroll-down-aggressively 0.01)
+(setq auto-window-vscroll nil)
+(setq fast-but-imprecise-scrolling nil)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+;; Horizontal Scroll
+(setq hscroll-step 1)
+(setq hscroll-margin 1)
+
+(provide 'init-ui-config)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-ui-config.el ends here
