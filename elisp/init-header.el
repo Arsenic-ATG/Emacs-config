cf485599a79230197428e8e40bde194c0e445cec
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-07-09 20:52:51 +0530

    minor indent fix

--- elisp/init-header.el
+++ elisp/init-header.el
@@ -49,7 +49,8 @@
   :load-path (lambda () (expand-file-name "non-melpa-elisp/header2" user-emacs-directory))
   :custom
   (header-copyright-notice (concat "Copyright (C) 2019 " (user-full-name) "\n"))
-  :hook (emacs-lisp-mode . auto-make-header)
+  :hook
+  (emacs-lisp-mode . auto-make-header)
   :config
   (add-to-list 'write-file-functions 'auto-update-file-header)
   (autoload 'auto-make-header "header2")
