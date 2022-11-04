8f419c09c3a6147cabbc58acf46eb3f81add333e
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-11-04 16:23:59 +0530

    Doom modeline: Remove minor modes from the modeline

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -87,7 +87,6 @@
   :custom
   ;; Don't compact font caches during GC. Windows Laggy Issue
   (inhibit-compacting-font-caches t)
-  (doom-modeline-minor-modes t)
   (doom-modeline-icon t)
   (doom-modeline-major-mode-color-icon t)
   (doom-modeline-height 15)
