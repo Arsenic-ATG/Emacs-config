f97da2ba9196238fd6d542023a2037182b286d8e
Ankur <arsenic.secondary@gmail.com>
2023-05-18 20:51:44 +0530

    new package: dumb jump
    dumb jump package to enable basic jump to definition functionality
    when LSP or tags are not present.

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 ;; Version: 1.0
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 85
+;;     Update #: 88
 ;; URL: https://github.com/Arsenic-ATG/Emacs-config
 ;; Keywords: init .emacs.d
 ;; Compatibility: emacs-version >= 26.1
@@ -133,6 +133,8 @@
 (require 'init-company)
 
 (require 'init-expand-region)
+
+(require 'init-dumb-jump)
 
 ;;;;;;;;;;;;;;;;;
 ;; Programming ;;
